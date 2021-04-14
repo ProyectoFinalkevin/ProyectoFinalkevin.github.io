defd55c327cc08b993923dc3ae41b25496d1ec13
ProyectoFinalkevin <82486207+ProyectoFinalkevin@users.noreply.github.com>
2021-04-13 21:56:08 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,11 +8,11 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyB2SD3RwDHuBtR7ytHun2dKtxD5j1m65XQ",
-    authDomain: "kevinproyecto43.firebaseapp.com",
-    projectId: "kevinproyecto43",
-    storageBucket: "kevinproyecto43.appspot.com",
-    messagingSenderId: "230196695011",
-    appId: "1:230196695011:web:678f03d1d121cdf4b85ad6",
-    measurementId: "G-MGDRDPR9JC"
+    apiKey: "AIzaSyD8X_H1mPTgsx5L-DxDOcBuZBRwYGrImJk",
+    authDomain: "proyectofinalkevin43.firebaseapp.com",
+    projectId: "proyectofinalkevin43",
+    storageBucket: "proyectofinalkevin43.appspot.com",
+    messagingSenderId: "652718178708",
+    appId: "1:652718178708:web:46fe5a9fdd913865ce0f81",
+    measurementId: "G-C4FB6V6HYY"
 });
